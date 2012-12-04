d6622b1bbd4667037cd20f2653fcb6d1316d067e
Jason Denney <jdenney@lgscout.com>
2012-12-04 17:40:21 -0500

    Add User Profile migration

---
+++ db/migrate/20121204223403_create_user_profile.rb
@@ -0,0 +1,18 @@
+class CreateUserProfile < ActiveRecord::Migration
+  def change
+    create_table :user_profiles do |t|
+      t.integer "user_id"
+      t.string "first_name"
+      t.string "last_name"
+      t.string "addrStreet1"
+      t.string "addrStreet2"
+      t.string "addrCity"
+      t.string "addrState"
+      t.string "addrZip"
+      t.string "phone"
+      t.string "email"
+      t.timestamps
+    end
+  end
+
+end
