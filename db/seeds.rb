c99d1e5f5572f414ac246dd90d1b72f1bc548ce8
Jason Denney <jdenney@lgscout.com>
2012-12-07 10:39:13 -0500

    Can now seed via rake db:seed
    Only using fixtures for seeding, using factories for testing.  
    -For some reason, module fixtures won't auto create dates, must include updated_at and created_at dates in fixture.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'active_record/fixtures'
+
+Dir.glob(File.join(Rails.root, 'test', 'fixtures', '**/*.{yml,csv}')).each do |fixture_file, something|
+  puts fixture_file
+  puts File.dirname(fixture_file)
+  ActiveRecord::Fixtures.create_fixtures(File.dirname(fixture_file), File.basename(fixture_file, '.*'))
+end
 
 if Rails.env.development?
-  FactoryGirl.create(:user)
+  user = FactoryGirl.build(:user)
+  FactoryGirl.create(:user) if not User.find_by_email(user.email)
 end
