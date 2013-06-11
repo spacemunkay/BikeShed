0f89cf057a8d526817c1400577b678030ba71283
Jason Denney <jasondenney@gmail.com>
2013-06-11 00:34:45 -0400

    Display User roles by username, then full name
    Also set the default_fields_for_forms

--- app/components/user_role_joins.rb
+++ app/components/user_role_joins.rb
@@ -5,12 +5,24 @@ class UserRoleJoins < Netzke::Basepack::Grid
     c.header = false
     c.title = "User Roles"
     c.columns = [
-      { :name => :user__first_name, :text => "First"},
-      { :name => :user__last_name, :text => "Last"},
+      { :name => :user__username, :text => "Username"},
+      { :name => :name, :getter => lambda{ |rec|
+                                                user = User.find_by_id(rec.user_id)
+                                                user.nil? ? "" : "#{user.first_name} #{user.last_name}"
+                                              }
+      },
       { :name => :role__role, :text => "Role"},
       :created_at,
       :updated_at,
       :ends ]
+  end
+
+  def default_fields_for_forms
+    [
+      { :name => :user__username, :field_label => 'Username' },
+      { :name => :role__role, :field_label => 'Role' },
+      { :name => :ends }
+    ]
   end
 
   #override with nil to remove actions
