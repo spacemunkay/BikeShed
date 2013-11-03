4a67ebe77e1b5becd3b2d8ba099ab6031c160dff
Jason Denney <jdenney@lgscout.com>
2013-11-03 11:34:48 -0500

    Disable sub panels until item clicked

--- app/components/customers/javascripts/init_component.js
+++ app/components/customers/javascripts/init_component.js
@@ -4,7 +4,6 @@
     this.callParent();
     this.getView().on('itemclick', function(view, record){
       // The beauty of using Ext.Direct: calling 3 endpoints in a row, which results in a single call to the server!
-      console.log("user: " + record.get('id') );
       this.selectCustomer({customer_id: record.get('id'), customer_type: 'Customer'});
     }, this);
   }
