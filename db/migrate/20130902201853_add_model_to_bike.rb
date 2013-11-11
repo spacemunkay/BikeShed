076a59ab458ee065e5acc05bd40946efa875919a
Jason Denney <jdenney@lgscout.com>
2013-11-11 17:27:51 -0500

    Make migration robust to model changes

--- db/migrate/20130902201853_add_model_to_bike.rb
+++ db/migrate/20130902201853_add_model_to_bike.rb
@@ -3,8 +3,7 @@ class AddModelToBike < ActiveRecord::Migration
     add_column :bikes, :model, :string
     change_column :bikes, :bike_model_id, :integer, :null => true
     Bike.all.each do |bike|
-      bike.model = BikeModel.find_by_id(bike.bike_model_id).model
-      bike.save
+      bike.update_attribute(:model, BikeModel.find_by_id(bike['bike_model_id']).model )
     end
   end
 end
