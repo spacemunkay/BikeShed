a8bdb52536ffa640a0396500e8d6d62fb36e625d
Jason Denney <jasondenney@gmail.com>
2014-01-19 19:32:18 -0500

    Improved Add Bike view
    Worked out some bootstrap overrides issues

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,6 +1,10 @@
 class BikesController < AuthenticatedController
 
   def new
+    @brands = BikeBrand.all.map{ |b| [b.brand, b.id] }
+    @brands.unshift( ["Select a brand", -1] )
+    @wheel_sizes = BikeWheelSize.all.map{ |w| [w.display_string, w.id] }
+    @wheel_sizes.unshift( ["Select a wheel size", -1] )
   end
 
 end
