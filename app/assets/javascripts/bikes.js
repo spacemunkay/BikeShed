2d09aba26af38a0b3fd6baa9962ddeff5e1f636d
Jason Denney <jasondenney@gmail.com>
2014-02-08 14:21:20 -0500

    Added submit js to bikes/new view
    * Added displayFormErrors utility
    * Added 4th option to bike styles “OTHER”

--- app/assets/javascripts/bikes.js
+++ app/assets/javascripts/bikes.js
@@ -1,1 +1,30 @@
 $('.btn').button();
+
+$("#add_bike_submit").click(function(){
+
+  json_data = { bike: {
+      serial_number:      $("#serial_number").val(),
+      bike_brand_id:      parseInt($("#bike_brand_id").val()),
+      shop_id:            parseInt($("#shop_id").val()),
+      model:              $("#model").val(),
+      bike_style_id:      parseInt($('input[name=bike_style]:checked').val()),
+      seat_tube_height:   parseInt($("#seat_tube_height").val()),
+      bike_condition_id:  parseInt($('input[name=bike_condition]:checked').val()),
+      bike_purpose_id:    1,
+      bike_wheel_size_id: parseInt($("#bike_wheel_size_id").val()),
+    }};
+
+  $.ajax({
+    url: $("#add_bike_submit").data("url"),
+    type: "POST",
+    data: json_data,
+    dataType: "json",
+    success: function(data, status, xhr){
+      //window.location = "";
+    },
+    error: function(data, status ){
+      displayFormErrors(data.responseJSON);
+    }
+  });
+
+});
