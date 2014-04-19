2d65ebb45faefb49d7c8347811a31a3d525014c4
Jason Denney <jasondenney@gmail.com>
2014-04-19 16:22:54 -0400

    JS Formatting

--- app/assets/javascripts/time_entries.js
+++ app/assets/javascripts/time_entries.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
+  var currentdate = new Date();
+  $("#date_id").datepicker().on('changeDate', function(ev){
+    $("#date_id").datepicker('hide');
+  });
+  $("#date_id").datepicker('setValue', currentdate);
 
-var currentdate = new Date();
-$("#date_id").datepicker().on('changeDate', function(ev){
-  $("#date_id").datepicker('hide');
-});
-$("#date_id").datepicker('setValue', currentdate);
-
-$("#start_time_id").timepicker();
-$("#end_time_id").timepicker();
+  $("#start_time_id").timepicker();
+  $("#end_time_id").timepicker();
 });
