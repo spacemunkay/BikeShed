a4b6ca12f969e0dd3eeb6d87aed9073db5457a48
Brigitte Warner <brigitte.warner@gmail.com>
2014-08-14 21:08:17 -0400

    define bike on all authenticated pages

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,3 +1,10 @@
 class AuthenticatedController < ApplicationController
   before_filter :authenticate_user!
+
+  before_filter :get_user_bike
+
+  def get_user_bike
+  	@bike = current_user.bike
+  end
+
 end
