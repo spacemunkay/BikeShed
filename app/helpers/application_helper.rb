e775c97fd1eae376129a4ca0fc381c7b9028ded9
Ilya Konanykhin <ilya.konanykhin@gmail.com>
2016-12-22 21:45:21 +0600

    BS3: new time entry.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
     end
     link_to(name, '#', id: "add_#{association.to_s.singularize}" , class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
   end
+
+  def link_to_dashboard
+    content_tag :p do
+      content_tag :a, href: root_path, class: 'btn btn-default', title: 'Back to dashboard' do
+        content_tag :span, '', class: 'glyphicon glyphicon-home'
+      end
+    end
+  end
 end
