3c521eba153f0dad837b03cd56ad3da6593dfc45
Ilya Konanykhin <ilya.konanykhin@gmail.com>
2016-12-22 23:23:48 +0600

    BS3: js validation errors.

--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -1,7 +1,7 @@
 function displayFormErrors(data){
-  if( data.errors != undefined ){
-    $.each( data.errors, function( field, errorMsg) {
-      $("#"+field).parents(".control-group").addClass("error");
+  if(data.errors != undefined ){
+    $.each(data.errors, function(field, errorMsg) {
+      $("#"+field).closest(".form-group").addClass("has-error");
       $("#"+field).siblings(".help-block").html(errorMsg);
     });
   }
